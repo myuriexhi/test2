ce0002415016815e7dc3bfab9f52579f7cd681c6
mayuri <mayuri03.exhibyte@gmail.com>
2021-10-18 15:22:08 +0530

    remove file

--- test5.js
+++ test5.js
@@ -2,7 +2,6 @@
 let arr = [1, 2, 3, 4, 5];
 
 function countTriplets(n, sum) {
-  // Initialize result
   let ans = 0;
 
   for (let i = 0; i < n - 2; i++) {
@@ -18,6 +17,7 @@ function countTriplets(n, sum) {
 let sum = 12;
 console.log(countTriplets(arr.length, sum));
 */
+
 /*
 function CountTriplets(arr, queries) {
   let cnt = 0;
@@ -32,35 +32,47 @@ function CountTriplets(arr, queries) {
 }
 
 let arr = [1, 2, 3, 4, 5];
-let queries = [
-  [1, 2, 4],
-  [2, 4, 3],
-  [1, 1, 1],
-];
 
 let n = arr.length;
 
 console.log(CountTriplets(arr.length, queries.length));
 */
 
-// const arr = [1, 2, 3, 4, 5];
-// const queries = [
-//   [1, 2, 4],
-//   [2, 4, 3],
-//   [1, 1, 1],
-// ];
-// function findTheTriples(arr, queries) {
-//   let count = 0;
-//   let n = arr.length;
-//   let m = queries.length;
+const Arr = [1, 2, 2, 1, 2, 1, 2];
+const queries = [
+  [1, 1, 2],
+  [1, 2, 1],
+];
+let n = Arr.length;
+let m = queries.length;
+let findTheTriples = [0];
 
-//   for (let i = 0; i < n; i++) {
-//     for (let j = i + 1; j < n; j++) {
-//       for (let k = j + 1; k < n; k++)
-//         if (arr[k] < arr[i] && arr[i] < arr[j]) {
-//           count += 1;
-//         }
-//     }
-//   }
-// }
-// console.log(findTheTriples(arr.length, queries.length));
+function countTriplets(x1, x2, x3) {
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        let y1 = Arr[i];
+        let y2 = Arr[j];
+        let y3 = Arr[k];
+        if (x1 == y1) {
+          if (x2 == y2) {
+            if (x3 == y3) {
+              ans = ans + 1;
+            }
+          }
+        }
+      }
+    }
+  }
+  return ans;
+}
+for (let i = 0; i < m; i++) {
+  let z1 = queries[i][0];
+  let z2 = queries[i][1];
+  let z3 = queries[i][2];
+
+  let Result = countTriplets(z1, z2, z3);
+  findTheTriples[i] = Result;
+}
+console.log(findTheTriples);
